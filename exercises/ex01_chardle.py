f935ba3c9ee3f79d68df752a08be4704280c30c5
Tamara Medina <arasymed@email.unc.edu>
2022-01-24 21:29:43 -0500

    corrections 2 ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -4,40 +4,41 @@ __author__ = "730435533"
 
 user_word: str = (input("Enter a 5-character word: "))
 user_word_characters: int = (len(user_word))
-if int(user_word_characters) != 5:
+if user_word_characters != 5:
     print("Error: Word must contain 5 characters")
     exit()
 
-single_character: str = input("Enter a single chracter: ")
+single_character: str = (input("Enter a single chracter: "))
 single_character_characters: int = (len(single_character))
-if int(single_character_characters) != 1:
+if single_character_characters != 1:
     print("Error: Character must be a single character.")
     exit()
 
 print("Searching for " + single_character + " in " + user_word)
 
-if single_character == (user_word[0] and user_word[1] and user_word[2] and user_word[3] and user_word[4]):
+
+if single_character == (user_word[0] + user_word[1] + user_word[2] + user_word[3] + user_word[4]):
     print(single_character + " found at index 0")
     print(single_character + " found at index 1")
     print(single_character + " found at index 2")
     print(single_character + " found at index 3")
     print(single_character + " found at index 4")
-    print("5 instances of " + single_character + " found in " + user_word)
+    print("4 instances of " + single_character + " found in " + user_word)
 else: 
-    if single_character == (user_word[0] and user_word[1] and user_word[2] and user_word[3]):
+    if single_character == (user_word[0] + user_word[1] + user_word[2] + user_word[3]):
         print(single_character + " found at index 0")
         print(single_character + " found at index 1")
         print(single_character + " found at index 2")
         print(single_character + " found at index 3")
         print("4 instances of " + single_character + " found in " + user_word)
     else: 
-        if single_character == (user_word[0] and user_word[1] and user_word[2]):
+        if single_character == (user_word[0] + user_word[1] + user_word[2]):
             print(single_character + " found at index 0")
             print(single_character + " found at index 1")
             print(single_character + " found at index 2")
             print("3 instances of " + single_character + " found in " + user_word)
         else: 
-            if single_character == (user_word[0] and user_word[1]):
+            if single_character == (user_word[0] + user_word[1]):
                 print(single_character + " found at index 0")
                 print(single_character + " found at index 1")
                 print("2 instances of " + single_character + " found in " + user_word)
@@ -46,20 +47,20 @@ else:
                     print(single_character + " found at index 0")
                     print("1 instance of " + single_character + " found in " + user_word)
                 else:
-                    if single_character == (user_word[1] and user_word[2] and user_word[3] and user_word[4]):
+                    if single_character == (user_word[1] + user_word[2] + user_word[3] + user_word[4]):
                         print(single_character + " found at index 1")
                         print(single_character + " found at index 2")
                         print(single_character + " found at index 3")
                         print(single_character + " found at index 4")
                         print("4 instances of " + single_character + " found in " + user_word)
                     else:
-                        if single_character == (user_word[1] and user_word[2] and user_word[3]):
+                        if single_character == (user_word[1] + user_word[2] + user_word[3]):
                             print(single_character + " found at index 1")
                             print(single_character + " found at index 2")
                             print(single_character + " found at index 3")
                             print("3 instances of " + single_character + " found in " + user_word)
                         else:
-                            if single_character == (user_word[1] and user_word[2]):
+                            if single_character == (user_word[1] + user_word[2]):
                                 print(single_character + " found at index 1")
                                 print(single_character + " found at index 2")
                                 print("2 instances of " + single_character + " found in " + user_word)
@@ -68,13 +69,13 @@ else:
                                     print(single_character + " found at index 1")
                                     print("1 instance of " + single_character + " found in " + user_word)
                                 else:
-                                    if single_character == (user_word[2] and user_word[3] and user_word[4]):
+                                    if single_character == (user_word[2] + user_word[3] + user_word[4]):
                                         print(single_character + " found at index 2")
                                         print(single_character + " found at index 3")
                                         print(single_character + " found at index 4")
                                         print("3 instances of " + single_character + " found in " + user_word)
                                     else:
-                                        if single_character == (user_word[2] and user_word[3]):
+                                        if single_character == (user_word[2] + user_word[3]):
                                             print(single_character + " found at index 2")
                                             print(single_character + " found at index 3")
                                             print("2 instances of " + single_character + " found in " + user_word)
@@ -83,7 +84,7 @@ else:
                                                 print(single_character + " found at index 2")
                                                 print("1 instance of " + single_character + " found in " + user_word)
                                             else:
-                                                if single_character == (user_word[3] and user_word[4]):
+                                                if single_character == (user_word[3] + user_word[4]):
                                                     print(single_character + " found at index 3")
                                                     print(single_character + " found at index 4")
                                                     print("2 instances of " + single_character + " found in " + user_word)
