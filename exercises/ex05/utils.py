bfcc1257c003977217c201a5b323e28e48b12546
Tamara Medina <arasymed@email.unc.edu>
2022-03-02 13:09:50 -0500

    ex05 2

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -6,13 +6,53 @@ __author__ = "730435533"
 
 
 def only_evens(integer_list: list[int]) -> list[int]:
-    even_list: list[int] = list()
-    i: int = 0
-    while i <= len(integer_list):
-        even: int = (integer_list[i] // 2) * 2
-        if integer_list[i] != even:
-            even_list.pop(integer_list[i])
-        else:
+    """Give a list of integers function returns only the even numbers."""
+    even_list: list[int] = []
+    # in order to append we need to place the item that we want to append
+    i: int = 0 
+    for item in integer_list:
+        # Checking the items in the list to see if they are even numbers
+        even: int = (item // 2) * 2
+# If after the multiplication (*2) the number is still the same it means that it is even
+        if even == item:
             even_list.append(integer_list[i])
-    i += 1
+        i += 1
     return even_list
+
+
+def sub(list_of_integers: list[int], start_index: int, end_index: int) -> list[int]:
+    subset_list: list[int] = []
+    i: int = start_index
+    if start_index > 0:
+        if end_index < len(list_of_integers):
+            while i >= start_index and i < end_index:
+                subset_list.append(list_of_integers[i])
+                i += 1
+            return subset_list
+        if end_index > len(list_of_integers):
+            while i >= start_index and i <= len(list_of_integers) - 1:
+                subset_list.append(list_of_integers[i])
+                i += 1
+        return subset_list
+    if start_index < 0:
+        i = 0
+        while i >= start_index and i < end_index:
+            subset_list.append(list_of_integers[i])
+            i += 1
+        return subset_list
+    if start_index == 0:
+        while i >= start_index and i < end_index:
+            subset_list.append(list_of_integers[i])
+            i += 1
+        return subset_list
+    if len(list_of_integers) == 0 or start_index > len(list_of_integers) or end_index <= 0:
+        return subset_list
+
+
+def concat(integer_list_one: list[int], integer_list_two: list[int]) -> list[int]:
+    list_with_all: list[int] = []
+    for item in integer_list_one:
+        list_with_all.append(item)
+    for item_list_two in integer_list_two:
+        list_with_all.append(item_list_two)
+    return list_with_all
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,3 +1,111 @@
 "Unit testing for lists utils functions."
 
 __author__ = "730435533"
+
+from utils import only_evens, sub, concat
+
+
+# ONLY_EVENS TESTS
+# Edge case:
+def test_only_evens_one_even() -> None:
+    integer_list: list[int] = []
+    assert only_evens(integer_list) == []
+
+
+def test_only_evens_no_evens() -> None:
+    integer_list: list[int] = [3, 7, 11]
+    assert only_evens(integer_list) == []
+
+
+# Usage case
+def test_only_evens_with_addition() -> None:
+    integer_list: list[int] = [22 + 1, 2 + 3, 1 + 2, 3 + 4, 2 + 2, 5 + 5]
+    assert only_evens(integer_list) == [4, 10]
+
+
+# Usage case
+def test_only_evens_with_multiplication() -> None:
+    integer_list: list[int] = [2 * 2, 3 * 9, 8 * 7, 10 * 2]
+    assert only_evens(integer_list) == [4, 56, 20]
+
+
+def test_only_evens_all_evens() -> None:
+    integer_list: list[int] = [4, 4, 4]
+    assert only_evens(integer_list) == [4, 4, 4]
+
+
+# SUB TESTS
+# Usage cases:
+def test_sub() -> None:
+    # if we wanted to take have one single number in our list:
+    list_of_integers: list[int] = [15, 45, 35, 14]
+    start_index: int = 1
+    end_index: int = 2
+    assert sub(list_of_integers, start_index, end_index) == [45]
+
+
+def test_sub_list_two_items() -> None:
+    # if we wanted to use the numbers with a single digit in a list:
+    list_of_integers: list[int] = [15, 45, 35, 14, 2, 3, 44, 78, 199, 202]
+    start_index: int = 4
+    end_index: int = 6
+    assert sub(list_of_integers, start_index, end_index) == [2, 3]
+
+
+# edge cases:
+# if someone wanted to have the same list as the return value:
+def test_sub_replicated_list() -> None:
+    list_of_integers: list[int] = [15, 45, 22, 89, 23, 15, 22]
+    start_index: int = 0
+    end_index: int = 7
+    assert sub(list_of_integers, start_index, end_index) == [15, 45, 22, 89, 23, 15, 22]
+
+
+def test_sub_negative_start_index() -> None:
+    list_of_integers: list[int] = [15, 45, 35, 14, 90, 15, 22]
+    start_index: int = -1
+    end_index: int = 4
+    assert sub(list_of_integers, start_index, end_index) == [15, 45, 35, 14]
+
+
+def test_sub_end_index_greater_than_length() -> None:
+    list_of_integers: list[int] = [99, 95, 34, 88, 2, 22]
+    start_index: int = 2
+    end_index: int = 10
+    assert sub(list_of_integers, start_index, end_index) == [34, 88, 2, 22]
+
+
+def test_sub_list_length_zero() -> None:
+    list_of_integers: list[int] = []
+    start_index: int = 2
+    end_index: int = 4
+    assert sub(list_of_integers, start_index, end_index) == []
+
+
+def test_sub_start_index_greater_than_length() -> None:
+    list_of_integers: list[int] = [2, 4, 5]
+    start_index: int = 10
+    end_index: int = 4
+    assert sub(list_of_integers, start_index, end_index) == []
+
+
+def test_sub_end_index_negative() -> None:
+    list_of_integers: list[int] = [21, 56, 66, 20]
+    start_index: int = 2
+    end_index: int = -4
+    assert sub(list_of_integers, start_index, end_index) == []
+
+
+def test_sub_end_index_zero() -> None:
+    list_of_integers: list[int] = [22, 54, 78, 2]
+    start_index: int = 2
+    end_index: int = 0
+    assert sub(list_of_integers, start_index, end_index) == []
+
+
+# CONCAT TESTS
+# Usage cases:
+def tes_concat_two_lists_together() -> None:
+    integer_list_one: list[int] = [1, 2, 3, 7, 9]
+    integer_list_two: list[int] = [10, 12, 14, 16]
+    assert concat(integer_list_one, integer_list_two) == [1, 2, 3, 7, 9, 10, 12, 14, 16]
